7878fdeccb3a298df049c083325e49b51cd52b9a
tokyo4j <hrak1529@gmail.com>
2024-08-17 18:48:21 +0900

    Handle arrays in more yaml way

--- README.md
+++ README.md
@@ -1,4 +1,206 @@
-Usage:
+## Usage
 
     yaml2xml < rc.yaml
 
+## Example:
+
+rc.yaml:
+
+```yaml
+core:
+  xwaylandPersistence: yes
+
+placement:
+  policy: cascade
+
+theme:
+  dropShadows: yes
+  font:
+    - place: ActiveWindow
+      weight: normal
+    - place: InactiveWindow
+      weight: normal
+
+windowSwitcher:
+  show: yes
+  fields:
+    field:
+      - content: type
+        width: 15%
+      - content: title
+        width: 85%
+
+regions:
+  region:
+    - { name: top-left, x: 0%, y: 0%, width: 50%, height: 50% }
+    - { name: top-right, x: 50%, y: 0%, width: 50%, height: 50% }
+
+keyboard:
+  keybind:
+    - key: A-F4
+      action:
+        - name: Close
+    - key: W-s
+      action:
+        - name: Execute
+          command: Alacritty
+    - key: W-a
+      action:
+        - name: Execute
+          command: fuzzel
+
+mouse:
+  default:
+  context:
+    - name: Frame
+      mousebind:
+        - button: W-Left
+          event: Press # "action" is renamed to "event"
+          action:
+            - name: Raise
+            - name: Move
+        - button: W-Right
+          event: Drag # "action" is renamed to "event"
+          action:
+            - name: Resize
+        - button: A-Left
+          event: Press # "action" is renamed to "event"
+          # None
+        - button: A-Left
+          event: Press # "action" is renamed to "event"
+          # None
+        - button: A-Right
+          event: Press # "action" is renamed to "event"
+          # None
+        - button: A-Right
+          event: Press # "action" is renamed to "event"
+          # None
+
+libinput:
+  device:
+    - category: touchpad
+      naturalScroll: yes
+      tapAndDrag: no
+```
+
+rc.xml (formatted):
+
+```xml
+<?xml version="1.0"?>
+<labwc_config>
+  <core>
+    <xwaylandPersistence>yes</xwaylandPersistence>
+  </core>
+  <placement>
+    <policy>cascade</policy>
+  </placement>
+  <theme>
+    <dropShadows>yes</dropShadows>
+    <font>
+      <place>ActiveWindow</place>
+      <weight>normal</weight>
+    </font>
+    <font>
+      <place>InactiveWindow</place>
+      <weight>normal</weight>
+    </font>
+  </theme>
+  <windowSwitcher>
+    <show>yes</show>
+    <fields>
+      <field>
+        <content>type</content>
+        <width>15%</width>
+      </field>
+      <field>
+        <content>title</content>
+        <width>85%</width>
+      </field>
+    </fields>
+  </windowSwitcher>
+  <regions>
+    <region>
+      <name>top-left</name>
+      <x>0%</x>
+      <y>0%</y>
+      <width>50%</width>
+      <height>50%</height>
+    </region>
+    <region>
+      <name>top-right</name>
+      <x>50%</x>
+      <y>0%</y>
+      <width>50%</width>
+      <height>50%</height>
+    </region>
+  </regions>
+  <keyboard>
+    <keybind>
+      <key>A-F4</key>
+      <action>
+        <name>Close</name>
+      </action>
+    </keybind>
+    <keybind>
+      <key>W-s</key>
+      <action>
+        <name>Execute</name>
+        <command>Alacritty</command>
+      </action>
+    </keybind>
+    <keybind>
+      <key>W-a</key>
+      <action>
+        <name>Execute</name>
+        <command>fuzzel</command>
+      </action>
+    </keybind>
+  </keyboard>
+  <mouse>
+    <default></default>
+    <context>
+      <name>Frame</name>
+      <mousebind>
+        <button>W-Left</button>
+        <event>Press</event>
+        <action>
+          <name>Raise</name>
+        </action>
+        <action>
+          <name>Move</name>
+        </action>
+      </mousebind>
+      <mousebind>
+        <button>W-Right</button>
+        <event>Drag</event>
+        <action>
+          <name>Resize</name>
+        </action>
+      </mousebind>
+      <mousebind>
+        <button>A-Left</button>
+        <event>Press</event>
+      </mousebind>
+      <mousebind>
+        <button>A-Left</button>
+        <event>Press</event>
+      </mousebind>
+      <mousebind>
+        <button>A-Right</button>
+        <event>Press</event>
+      </mousebind>
+      <mousebind>
+        <button>A-Right</button>
+        <event>Press</event>
+      </mousebind>
+    </context>
+  </mouse>
+  <libinput>
+    <device>
+      <category>touchpad</category>
+      <naturalScroll>yes</naturalScroll>
+      <tapAndDrag>no</tapAndDrag>
+    </device>
+  </libinput>
+</labwc_config>
+```
--- yaml2xml.c
+++ yaml2xml.c
@@ -1,159 +1,122 @@
 // SPDX-License-Identifier: GPL-2.0-only
 #include <assert.h>
-#include <yaml.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include <yaml.h>
 
-static struct element *elements;
-static int nr_elements, alloc_elements;
-
-struct element {
-	char name[64];
-};
-
-static struct element *
-push(const char *name)
-{
-	if (nr_elements == alloc_elements) {
-		alloc_elements = (alloc_elements + 16) * 2;
-		elements = realloc(elements, alloc_elements * sizeof(struct element));
-		if (!elements) {
-			perror("out of memeory");
-			exit(EXIT_FAILURE);
-		}
-	}
-	struct element *element = elements + nr_elements;
-	memset(element, 0, sizeof(*element));
-	nr_elements++;
-	strncpy(element->name, name, sizeof(element->name));
-	element->name[sizeof(element->name) - 1] = '\0';
-	return element;
+__attribute__((unused)) static const char *
+get_event_name(yaml_event_type_t type) {
+    switch (type) {
+    case YAML_NO_EVENT:
+        return "YAML_NO_EVENT";
+    case YAML_STREAM_START_EVENT:
+        return "YAML_STREAM_START_EVENT";
+    case YAML_STREAM_END_EVENT:
+        return "YAML_STREAM_END_EVENT";
+    case YAML_DOCUMENT_START_EVENT:
+        return "YAML_DOCUMENT_START_EVENT";
+    case YAML_DOCUMENT_END_EVENT:
+        return "YAML_DOCUMENT_END_EVENT";
+    case YAML_ALIAS_EVENT:
+        return "YAML_ALIAS_EVENT";
+    case YAML_SCALAR_EVENT:
+        return "YAML_SCALAR_EVENT";
+    case YAML_SEQUENCE_START_EVENT:
+        return "YAML_SEQUENCE_START_EVENT";
+    case YAML_SEQUENCE_END_EVENT:
+        return "YAML_SEQUENCE_END_EVENT";
+    case YAML_MAPPING_START_EVENT:
+        return "YAML_MAPPING_START_EVENT";
+    case YAML_MAPPING_END_EVENT:
+        return "YAML_MAPPING_END_EVENT";
+    default:
+        return NULL;
+    }
 }
 
-static void
-pop(char *buffer, size_t len)
-{
-	strncpy(buffer, elements[nr_elements-1].name, len);
-	nr_elements--;
+static void process_mapping(yaml_parser_t *parser);
+
+static void process_sequence(yaml_parser_t *parser, char *key_name) {
+    while (1) {
+        yaml_event_t event;
+        assert(yaml_parser_parse(parser, &event));
+        if (event.type == YAML_SEQUENCE_END_EVENT)
+            break;
+        switch (event.type) {
+        case YAML_MAPPING_START_EVENT:
+            printf("<%s>", key_name);
+            process_mapping(parser);
+            printf("</%s>", key_name);
+            break;
+        case YAML_SEQUENCE_START_EVENT:
+            assert(false);
+            break;
+        case YAML_SCALAR_EVENT:
+            printf("<%s>", (char *)key_name);
+            printf("%s", (char *)event.data.scalar.value);
+            printf("</%s>", (char *)key_name);
+            break;
+        default:;
+        }
+    }
 }
 
-static void
-indent(int level)
-{
-	printf("%*c", 2 * level, ' ');
+static void process_mapping(yaml_parser_t *parser) {
+    while (1) {
+        yaml_event_t key_event, value_event;
+        assert(yaml_parser_parse(parser, &key_event));
+        if (key_event.type == YAML_MAPPING_END_EVENT)
+            break;
+        assert(key_event.type == YAML_SCALAR_EVENT);
+
+        assert(yaml_parser_parse(parser, &value_event));
+
+        switch (value_event.type) {
+        case YAML_MAPPING_START_EVENT:
+            printf("<%s>", (char *)key_event.data.scalar.value);
+            process_mapping(parser);
+            printf("</%s>", (char *)key_event.data.scalar.value);
+            break;
+        case YAML_SEQUENCE_START_EVENT:
+            process_sequence(parser, (char *)key_event.data.scalar.value);
+            break;
+        case YAML_SCALAR_EVENT:
+            printf("<%s>", (char *)key_event.data.scalar.value);
+            printf("%s", (char *)value_event.data.scalar.value);
+            printf("</%s>", (char *)key_event.data.scalar.value);
+            break;
+        default:;
+        }
+    }
 }
 
-void
-process_one_event(yaml_event_t *event)
-{
-	static int level;
-	static bool in_scalar;
-
-	switch (event->type) {
-	case YAML_NO_EVENT:
-	case YAML_ALIAS_EVENT:
-	case YAML_MAPPING_START_EVENT:
-	case YAML_MAPPING_END_EVENT:
-	case YAML_STREAM_START_EVENT:
-	case YAML_STREAM_END_EVENT:
-		break;
-	case YAML_DOCUMENT_START_EVENT:
-		printf("<?xml version=\"1.0\"?>\n");
-		printf("<labwc_config>\n");
-		++level;
-		break;
-	case YAML_DOCUMENT_END_EVENT:
-		printf("</labwc_config>\n");
-		--level;
-		break;
-	case YAML_SCALAR_EVENT:
-		/*
-		 * scalar-sequence -> <foo>
-		 *                      <child></child>
-		 *                      ...
-		 *                    </foo>
-		 *
-		 *     ...with the child-elements defined withing a
-		 *     subsequent mapping-{start,end}.
-		 *
-		 * scalar-scalar -> <foo>bar</foo>
-		 *
-		 *     ...where foo and bar are the names of the first and
-		 *     second scalars respectively.
-		 */
-		if (in_scalar) {
-			/* Handle second scalar of pair, so print "bar</foo>" */
-			char element[64] = { 0 };
-			pop(element, sizeof(element));
-			printf("%s</%s>\n", event->data.scalar.value, element);
-			in_scalar = false;
-		} else {
-			/*
-			 * Handle first scalar out of a potential pair, so
-			 * print "<foo>"
-			 */
-			char *value = (char *)event->data.scalar.value;
-			indent(level);
-			printf("<%s>", value);
-			push(value);
-			in_scalar = true;
-		}
-		break;
-	case YAML_SEQUENCE_START_EVENT:
-		/* Get ready for child-elements */
-		assert(in_scalar);
-		printf("\n");
-		++level;
-		in_scalar = false;
-		break;
-	case YAML_SEQUENCE_END_EVENT:
-		/*
-		 * We're now at the closing element </foo> of
-		 * <foo>
-		 *   <bar></bar>
-		 * </foo>
-		 */
-		indent(--level);
-		char element[64] = { 0 };
-		pop(element, sizeof(element));
-		printf("</%s>\n", element);
-		break;
-	}
-	if (level < 0) {
-		fprintf(stderr, "warn: indentation error\n");
-		level = 0;
-	}
+static void process_root(yaml_parser_t *parser) {
+    yaml_event_t event;
+    assert(yaml_parser_parse(parser, &event));
+    assert(event.type == YAML_STREAM_START_EVENT);
+    assert(yaml_parser_parse(parser, &event));
+    assert(event.type == YAML_DOCUMENT_START_EVENT);
+    assert(yaml_parser_parse(parser, &event));
+    assert(event.type == YAML_MAPPING_START_EVENT);
+    printf("<?xml version=\"1.0\"?><labwc_config>");
+    process_mapping(parser);
+    printf("</labwc_config>");
 }
 
-int
-main(int argc, char **argv)
-{
-	if (argc > 1) {
-		printf("usage: %s < input.yaml > output.xml\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
+int main(int argc, char **argv) {
+    if (argc > 1) {
+        printf("usage: %s < input.yaml > output.xml\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-	yaml_parser_t parser;
-	yaml_parser_initialize(&parser);
-	yaml_parser_set_input_file(&parser, stdin);
+    yaml_parser_t parser;
+    yaml_parser_initialize(&parser);
+    yaml_parser_set_input_file(&parser, stdin);
 
-	yaml_event_type_t event_type = 0;
-	while (event_type != YAML_STREAM_END_EVENT) {
-		yaml_event_t event;
-		int success = yaml_parser_parse(&parser, &event);
-		if (!success) {
-			fprintf(stderr, "fatal: yaml parse: %s\n", parser.problem);
-			yaml_parser_delete(&parser);
-			return EXIT_FAILURE;
-		}
-		process_one_event(&event);
-		event_type = event.type;
-		yaml_event_delete(&event);
-	}
-	yaml_parser_delete(&parser);
-	free(elements);
-	nr_elements = 0;
-	alloc_elements = 0;
-	return EXIT_SUCCESS;
+    // TODO: free objects
+    process_root(&parser);
+
+    yaml_parser_delete(&parser);
+    return EXIT_SUCCESS;
 }
